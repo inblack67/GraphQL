1ad58e089898cc7dddb576192d4456fdf0009208
Aman <devproddeploy@gmail.com>
2020-08-14 18:06:47 +0530

    author typr

--- schema/Schema.js
+++ schema/Schema.js
@@ -1,18 +1,33 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
-const data = [
+const books = [
     { id: '1', title: 'lorem', genre: 'oka' },
     { id: '2', title: 'ipsum', genre: 'not' },
     { id: '3', title: 'harry', genre: 'react' },
 ]
 
+const authors = [
+    { id: '1', name: 'lorem', age: 23 },
+    { id: '2', name: 'ipsum', age: 43 },
+    { id: '3', name: 'harry', age: 32 },
+]
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         title: { type: GraphQLString },
         genre: { type: GraphQLString },
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
     })
 })
 
@@ -21,9 +36,16 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },  // id will be string as usual, no mattered if entered as an int or string
             resolve: (parent, args) => {
-                return data.find(el => el.id === args.id);
+                return books.find(el => el.id === args.id);
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve: (parent, args) => {
+                return authors.find(el => el.id === args.id);
             }
         }
     }
